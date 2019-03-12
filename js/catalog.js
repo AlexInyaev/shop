46aaf3debe9a14defeba9cafb2e130a16570c9b4
AlexInyaev <46852628+AlexInyaev@users.noreply.github.com>
2019-03-13 00:53:09 +0300

    отформатировал код

--- js/catalog.js
+++ js/catalog.js
@@ -1,6 +1,21 @@
-var productsCatalog = [
-    {id:'el1', name:'CANON EOS R',         price:1000, img:'https://images.unsplash.com/photo-1552039609-7155637f3911?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'},
-    {id:'el2', name:'CANON EOS 6580',      price:2000, img:'https://images.unsplash.com/photo-1552116460-e43109570af0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'},
-    {id:'el3', name:'CANON EOS 5d Mark',   price:3000, img:'https://images.unsplash.com/photo-1552063377-9933fd2a6fe7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'},
-    {id:'el4', name:'Sony Cyber-shot DSC', price:4000, img:'https://images.unsplash.com/photo-1552028034-d2ffb9b298e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'}
-];
+var productsCatalog = [{
+	id: 'el1',
+	name: 'CANON EOS R',
+	price: 1000,
+	img: 'https://images.unsplash.com/photo-1552039609-7155637f3911?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+}, {
+	id: 'el2',
+	name: 'CANON EOS 6580',
+	price: 2000,
+	img: 'https://images.unsplash.com/photo-1552116460-e43109570af0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+}, {
+	id: 'el3',
+	name: 'CANON EOS 5d Mark',
+	price: 3000,
+	img: 'https://images.unsplash.com/photo-1552063377-9933fd2a6fe7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+}, {
+	id: 'el4',
+	name: 'Sony Cyber-shot DSC',
+	price: 4000,
+	img: 'https://images.unsplash.com/photo-1552028034-d2ffb9b298e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+}];
